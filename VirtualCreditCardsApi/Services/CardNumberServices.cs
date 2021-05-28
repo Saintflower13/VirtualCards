a8fff84ebb2095f345dd4ba6be3a14b25ff86cdc
Emanuelle CS Flôr <csf.emanuelle@gmail.com>
2021-05-28 14:34:54 -0300

    DEV: Alteração calcular dv.

--- VirtualCreditCardsApi/Services/CardNumberServices.cs
+++ VirtualCreditCardsApi/Services/CardNumberServices.cs
@@ -3,6 +3,50 @@ using VirtualCreditCardsApi.Models;
 
 namespace VirtualCreditCardsApi.Services
 {
+    public static class CheckDigitCalculator
+    {
+        private static bool IsEvenIndex(int index) => index % 2 == 0;
+
+        private static int SumDigits(int number)
+        {
+            string mult_str = Convert.ToString(number);
+            return (int)Char.GetNumericValue(mult_str[0]) + (int)Char.GetNumericValue(mult_str[1]);
+        }
+          /*      num = ler_inteiro_positivo();
+
+          while (num > 0) {
+            digito =  num % 10; // O resto é o algarismo mais a direita 
+            soma += digito; 
+            num = num / 10; // A parte inteira da divisão é o número sem o algarismo mais a direita 
+          }
+        */
+
+        private static int CalculateOddNumber(int digit)
+        {
+            digit = digit * 2;
+
+            if (digit > 9)
+                SumDigits(digit);
+
+            return digit;
+        }
+
+
+
+        public static int CalculateCheckDigit(string cardNumber)
+        {
+            int sum = 0, digit;
+
+            for (int i = 0; i < cardNumber.Length; i++)
+            {
+                digit = (int)Char.GetNumericValue(cardNumber[i]);
+                sum += IsEvenIndex(i + 1) ? digit : CalculateOddNumber(digit);
+            }
+
+            return 10 - (sum % 10);
+        }
+    }
+
     public static class CardNumberServices
     {
         private static string GenerateIssuerIdNumber(BaseCardIIN cardIIN)
@@ -16,44 +60,13 @@ namespace VirtualCreditCardsApi.Services
             return clientId.ToString().PadLeft(9, '0');
         }
 
-        private static bool IsEvenIndex(int index) => index % 2 == 0;
-
-        private static int CalculateOddNumber(int digit)
-        {
-            digit = digit * 2;
-
-            if (digit > 9)
-            {
-                string mult_str = Convert.ToString(digit);
-                digit = (int)Char.GetNumericValue(mult_str[0]) + (int)Char.GetNumericValue(mult_str[1]);
-            }   
-
-            return digit;
-        }
-
-        private static string CalculateDV(string cardNumber)
-        {
-            int digitsSum = 0;
-            int digit, dv = 0;
-
-            for (int i = 0; i < cardNumber.Length; i++)
-            {
-                digit = (int)Char.GetNumericValue(cardNumber[i]);
-
-                if (IsEvenIndex(i + 1))
-                    digitsSum += digit;
-                else
-                    digitsSum += CalculateOddNumber(digit);
-            }
-
-            dv = 10 - (digitsSum % 10);
-            return dv.ToString();
-        }
+        private static string GenerateCheckDigit(string cardNumber) => 
+            CheckDigitCalculator.CalculateCheckDigit(cardNumber).ToString();
 
         public static string GenerateCardNumber(BaseCardIIN cardIIN, int clientId)
         {
-            string cardNumber = GenerateIssuerIdNumber(cardIIN) + GenerateClientId(clientId);
-            return cardNumber + CalculateDV(cardNumber);
+            var cardNumber = GenerateIssuerIdNumber(cardIIN) + GenerateClientId(clientId);
+            return cardNumber + GenerateCheckDigit(cardNumber);
         }
     }
 }
